35eca67cf66458862bb5640dbcb62fadf6195e00
IMMANUEL <immanuelcherui1@gmail.com>
2025-05-14 10:10:00 +0300

    Graphics jsx

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -21,22 +21,22 @@ const Projects = () => {
                     ))}
                 </div>
                 <div>
-                <h1 className="section-title">Graphics</h1>
-                <div className="grid md:grid-cols-3 md:gap-8 sm:gap-8 gap-4">
-                    {projectsData.map((item, index) => (
+                    <h1 className="section-title">Websites</h1>
+                    <div className="grid md:grid-cols-3 md:gap-8 sm:gap-8 gap-4">
+                      {projectsData.filter(p => p.type === "website").map((item, index) => (
                         <div key={index} className='md:p-4 p-3 border border-dashed border-slate-500 shadow shadow-slate-700 rounded-md'>
-                            <div>
-                                <img className="rounded-md" src={item.image} alt="" />
+                          <div>
+                            <img className="rounded-md" src={item.image} alt="" />
+                          </div>
+                          <a href={item.link} target="_blank" rel="noopener noreferrer">
+                            <div className="md:pt-4 pt-2">
+                              <h3 className="mb-2 font-semibold">{item.name}</h3>
+                              <p className="font-light text-sm">{item.details}</p>
                             </div>
-                            <a href={item.link} target="_blank">
-                                <div className="md:pt-4 pt-2">
-                                    <h3 className="mb-2 font-semibold ">{item.name}</h3>
-                                    <p className="font-light text-sm">{item.details}</p>
-                                </div>
-                            </a>
+                          </a>
                         </div>
-                    ))}
-                </div>
+                      ))}
+                    </div>
                 <div className="md:mt-12 mt-4 flex justify-center items-center">
                     <a className="btn" href="https://github.com/immanuelcherui1" target='_blank' >More Projects</a>
                 </div>
