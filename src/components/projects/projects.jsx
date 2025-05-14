15dae6d8b7f5b646e38001e731f3ce0ae2b86c90
IMMANUEL <immanuelcherui1@gmail.com>
2025-05-14 09:37:28 +0300

    Update projects.jsx

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -4,7 +4,24 @@ const Projects = () => {
     return ( 
         <section id="projects">
             <div>
-                <h1 className="section-title">Some of my Work</h1>
+                <h1 className="section-title">Websites</h1>
+                <div className="grid md:grid-cols-3 md:gap-8 sm:gap-8 gap-4">
+                    {projectsData.map((item, index) => (
+                        <div key={index} className='md:p-4 p-3 border border-dashed border-slate-500 shadow shadow-slate-700 rounded-md'>
+                            <div>
+                                <img className="rounded-md" src={item.image} alt="" />
+                            </div>
+                            <a href={item.link} target="_blank">
+                                <div className="md:pt-4 pt-2">
+                                    <h3 className="mb-2 font-semibold ">{item.name}</h3>
+                                    <p className="font-light text-sm">{item.details}</p>
+                                </div>
+                            </a>
+                        </div>
+                    ))}
+                </div>
+                <div>
+                <h1 className="section-title">Graphics</h1>
                 <div className="grid md:grid-cols-3 md:gap-8 sm:gap-8 gap-4">
                     {projectsData.map((item, index) => (
                         <div key={index} className='md:p-4 p-3 border border-dashed border-slate-500 shadow shadow-slate-700 rounded-md'>
